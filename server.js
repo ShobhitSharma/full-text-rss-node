5a253ddf87baec25fbf0bb8cb0f6b83e14209224
ShobhitSharma <ishobhitsharma@gmail.com>
2013-10-25 02:18:14 +0200

    Modules added

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var RssGenerator = require("./lib/rss_generator").RssGenerator;
+
+app.use(express.logger());
+
+app.get('/', function(req, res) {
+  var rss = new RssGenerator();
+
+  rss.feeds(function(feeds){
+    res.charset = 'UTF-8';
+    res.header('Content-Type', 'text/xml');
+    res.header('Last-Modified', new Date().toString());
+    res.header('Cache-Control', 'no-cache');
+    res.send(feeds);
+  });
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Process is listening on " + port);
+});
